f525b1c06012324eaba869daefa001bb27e289fa
sierrakatow <sierra@avokatow.com>
2015-01-08 17:28:17 -0800

    fix

--- routes/comedy.js
+++ routes/comedy.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var request = require('request');
 var async = require('async'),
 	_ = require('underscore'),
-	moment = require('moment');
+	moment = require('moment-timezone');
 
 var googleurl = 'https://www.googleapis.com/';
-var time = moment().format();
+var time = moment().tz("America/Los_Angeles").format();
 var req_urls = {
 	calendar: 'calendar/v3/calendars/uttvadcqlbvehtnua65g4fuqks@group.calendar.google.com/events?singleEvents=true&orderBy=startTime&timeMin='+time+'&key=AIzaSyDqvITIZ5hTcYqf1M_Q8Ic6IqZ9yUzy9SU',
 	videos: 'youtube/v3/playlistItems?part=snippet&playlistId=PLgyUzo9KN5BaZKoCABNWfTSV8cs1simr8&key=AIzaSyDqvITIZ5hTcYqf1M_Q8Ic6IqZ9yUzy9SU'
